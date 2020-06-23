f138d1d60c184e0aa1a84c753837b9d6d4fa4ded
kevin1430 <nesskm27@uwgb.edu>
2020-06-23 20:27:39 +0000

    Created 6 variables for user input, added questions, and printed to console

--- Project1.java
+++ Project1.java
@@ -20,7 +20,7 @@ public class Project1 {
 
     System.out.println("Enter your first name and last name:");
     String name = user_input.nextLine();
-
+    //Questions from https://www.mockquestions.com/position/FBI/
     System.out.println("What are your top 3 strengths:");
     String threeStrengths = user_input.nextLine();
 
@@ -30,16 +30,19 @@ public class Project1 {
     System.out.println("How will you handle the inevitable stress and pressure that comes with working for the FBI?");
     String handleStress = user_input.nextLine();
 
+    System.out.println("The FBI hires less than 5% of our applicants. Why should we choose you?");
+    String chooseMe = user_input.nextLine();
+
+    System.out.println("When have you gone above and beyond the call of duty for another individual?");
+    String aboveAndBeyond = user_input.nextLine();
     System.out.println("\n\n\n\n");
 
     System.out.println("Name: " + name);
     System.out.println("Top Three Strengths: " + threeStrengths);
     System.out.println("Your Greatest Weakness: " + greatestWeakness);
     System.out.println("How you are able to handle stress and pressure: " + handleStress);
-    
-
-
-
+    System.out.println("Why we should choose you: " + chooseMe);
+    System.out.println("When you went above and beyond: " + aboveAndBeyond);
     System.out.println("Thank you for answering the questions.");
   }
 }
